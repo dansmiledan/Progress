284eb076466a154baabab5aff2b8b23d0df3be3f
dansmiledan <yntantan@icloud.com>
2022-05-09 16:37:02 +0800

    fib & squart-list

--- sicp/fib.rkt
+++ sicp/fib.rkt
@@ -2,8 +2,8 @@
 (define (fib-recursion n) (cond ((= n 0) 0) ((= n 1) 1) (else (+ (fib (- n 1)) (fib (- n 2))))))
 (define (fib-iter a b count) (if (= count 0) b (fib-iter (+ a b) a (- count 1))))
 (define (fib n) (fib-iter 1 0 n))
-
-
+; [a, b] * [[1, 1][1, 0]]
+; (define (fast-fib n))
 (fib-recursion 1)
 (fib 1)
 
