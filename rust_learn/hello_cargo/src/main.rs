91ee7c9c5d98702660a87b8645722c45ecba5faf
dansmiledan <yntantan@icloud.com>
2024-05-01 22:34:44 +0800

    chapter 13

--- rust_learn/hello_cargo/src/main.rs
+++ rust_learn/hello_cargo/src/main.rs
@@ -1,4 +1,4 @@
-fn longest<'a, 'b>(a: &'a str, b: &'b str) -> &'a 'b str {
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
 	if a.len() > b.len() {
 		return a;
 	} else {
@@ -7,10 +7,15 @@ fn longest<'a, 'b>(a: &'a str, b: &'b str) -> &'a 'b str {
 }
 fn main() {
 	let string1 = String::from("long string is long");
-	let r;
 	{
 		let string2 = String::from("xyz");
-		r = longest(string1.as_str(), string2.as_str());
 	}
-		println!("The longest string is {}", r);
+	let mut vec: Vec<i32> = Vec::new();
+	vec.push(4);
+	vec.push(3);
+	vec.push(6);
+	vec.sort();
+	for v in vec {
+		println!("{v}");
+	}
 }
