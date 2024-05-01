89ae36649b255a11e059439c10555ceee3f70112
dansmiledan <yntantan@icloud.com>
2024-05-01 20:36:02 +0800

    chapter 12

--- rust_learn/guess_game/src/main.rs
+++ rust_learn/guess_game/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+	println!("Guess a number!");
+	let secret_number = rand::thread_rng().gen_range(1..=100);
+	// println!("the number {secret_number}");
+	loop {
+		println!("input your guess: ");
+		let mut guess = String::new();
+		io::stdin()
+			.read_line(&mut guess)
+			.expect("Failed to readline");
+		println!("your guessed {guess}");
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		};
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("too small"),
+			Ordering::Greater => println!("too large"),
+			Ordering::Equal => {
+				println!("you win");
+				break;
+			}
+		}
+	}
 }
--- rust_learn/hello_cargo/src/main.rs
+++ rust_learn/hello_cargo/src/main.rs
@@ -1,3 +1,16 @@
+fn longest<'a, 'b>(a: &'a str, b: &'b str) -> &'a 'b str {
+	if a.len() > b.len() {
+		return a;
+	} else {
+		return b;
+	}
+}
 fn main() {
-    println!("Hello, world!");
+	let string1 = String::from("long string is long");
+	let r;
+	{
+		let string2 = String::from("xyz");
+		r = longest(string1.as_str(), string2.as_str());
+	}
+		println!("The longest string is {}", r);
 }
