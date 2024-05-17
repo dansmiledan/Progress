56367a0d913e3a19675861a0524ea097511a5570
dansmiledan <yntantan@icloud.com>
2024-05-17 22:45:35 +0800

    finish simple web server

--- rust_learn/chap2/simple_web_server_2_5/src/main.rs
+++ rust_learn/chap2/simple_web_server_2_5/src/main.rs
@@ -1,9 +1,11 @@
 use actix_web::{web, App, HttpResponse, Responder, HttpServer};
+use serde::Deserialize;
 
 #[actix_web::main]
 async fn main() {
     let server = HttpServer::new(|| {
         App::new().route("/", web::get().to(get_index))
+		.route("/gcd", web::post().to(post_gcd))
     });
 
     println!("serve on http://localhost:3000");
@@ -25,4 +27,33 @@ async fn get_index() -> impl Responder {
 		</form>
 		"#,
 	)
+}
+
+
+async fn post_gcd(form: web::Form<GcdParamter>) -> HttpResponse {
+	if form.n == 0 || form.m == 0 {
+		return HttpResponse::BadRequest().content_type("text/html").body("computing gcd with zero is boring");
+	}
+	let response = format!(
+		"The greatest common divisor of the number {} and {} is <b>{}</b>\n", form.n, form.m, gcd(form.n, form.m)
+	);
+	HttpResponse::Ok().content_type("text/html").body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+	while m != 0 {
+		if m < n {
+			let t = m;
+			m = n;
+			n = t;
+		}
+		m = m % n;
+	}
+	return n;
+}
+
+#[derive(Deserialize)]
+struct GcdParamter {
+	n: u64,
+	m: u64
 }
